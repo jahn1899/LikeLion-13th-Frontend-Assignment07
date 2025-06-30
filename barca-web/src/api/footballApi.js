1809dfa574f21663c5110d7c76710b33d977d234
jian1899 <06jahn0916@gmail.com>
2025-06-30 17:45:06 +0900

    barca-web

--- barca-web/src/api/footballApi.js
+++ barca-web/src/api/footballApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api-football-v1.p.rapidapi.com/v3';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const HEADERS = {
   'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
   'X-RapidAPI-Host': import.meta.env.VITE_API_HOST,
